7012e420cb71650923d635f9d9165ab443097dab
MikeGoz <gozdz@onet.pl>
2017-09-11 23:16:59 +0200

    poprawka Task 1.5

---
+++ index.js
@@ -0,0 +1,28 @@
+// task 1.5
+
+var OSinfo = require('./modules/osinfo');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+
+  if (input !== null) {                               
+    var instruction = input.toString().trim();
+    var version = process.versions.node;
+
+    switch (instruction) {
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case '/node':
+				process.stdout.write('Node version: '+ version +'\n');
+        break;
+      case '/system':
+    		OSinfo.print();
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    }
+  }
+});
---
+++ modules/timeformat.js
@@ -0,0 +1,16 @@
+// task 1.5
+
+function timeFormat(tm) {
+  var sec = Math.floor((tm % 3600) % 60);
+	var min = Math.floor((tm % 3600) / 60);
+  var hrs = Math.floor(tm / 3600);
+
+	if (tm < 60) {
+    return(sec + ' sec.');
+  } else if (tm < 3600) {
+      return(min + ' min. ' + sec + ' sec.');
+ 	} else {
+    	return(hrs + ' hrs. ' + min + ' min. ' + sec + ' sec.');
+  }
+}
+exports.print = timeFormat;
