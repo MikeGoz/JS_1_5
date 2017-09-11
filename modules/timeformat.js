f3fac0093bd6d908b7b113d8016e5320c5963a6f
Mike <gozdz@onet.pl>
2017-09-11 23:25:43 +0200

    Poprawka Task 1.5

--- modules/timeformat.js
+++ modules/timeformat.js
@@ -2,15 +2,15 @@
 
 function timeFormat(tm) {
   var sec = Math.floor((tm % 3600) % 60);
-	var min = Math.floor((tm % 3600) / 60);
+  var min = Math.floor((tm % 3600) / 60);
   var hrs = Math.floor(tm / 3600);
 
-	if (tm < 60) {
-    return(sec + ' sec.');
+  if (tm < 60) {
+     return(sec + ' sec.');
   } else if (tm < 3600) {
-      return(min + ' min. ' + sec + ' sec.');
- 	} else {
-    	return(hrs + ' hrs. ' + min + ' min. ' + sec + ' sec.');
+     return(min + ' min. ' + sec + ' sec.');
+  } else {
+    return(hrs + ' hrs. ' + min + ' min. ' + sec + ' sec.');
   }
 }
 exports.print = timeFormat;
