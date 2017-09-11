85174c64f9d6cb390f4ad27c625e000746c3d94a
Mike <gozdz@onet.pl>
2017-09-11 23:23:47 +0200

    poprawka Task 1.5

--- modules/osinfo.js
+++ modules/osinfo.js
@@ -6,21 +6,21 @@ var colors = require('colors'); // w konsoli Git Basch nie wyświetla kolorów, 
 
 function getOSinfo() {
   var type = os.type();
-	var release = os.release();
+  var release = os.release();
   var cpu = os.cpus()[0].model;
-	var userInfo = os.userInfo();
-	var uptime = os.uptime();      // method returns the system uptime in number of seconds.
+  var userInfo = os.userInfo();
+  var uptime = os.uptime();      // method returns the system uptime in number of seconds.
 
-	if (type === 'Darwin') {
-    	type = 'OSX';
-	} else if (type === 'Windows_NT') {
-    	type = 'Windows';
-	}
-	console.log('Uptime: '.grey, timeFormat.print(uptime));
-	console.log('System: '.red, type);
-	console.log('Release: '.blue, release);
+  if (type === 'Darwin') {
+    type = 'OSX';
+  } else if (type === 'Windows_NT') {
+    type = 'Windows';
+  }
+  console.log('Uptime: '.grey, timeFormat.print(uptime));
+  console.log('System: '.red, type);
+  console.log('Release: '.blue, release);
   console.log('CPU model: '.green, cpu);
   console.log('User name: '.yellow, userInfo.username);
-	console.log('Home dir: '.white, userInfo.homedir);
+  console.log('Home dir: '.white, userInfo.homedir);
 }
 exports.print = getOSinfo;
